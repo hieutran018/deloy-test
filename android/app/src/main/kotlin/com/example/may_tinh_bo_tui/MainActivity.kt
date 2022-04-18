04742572899e3c2f35c3f13012f9b6a557fa22eb
25042001iueH <hieutdc@antoree.com>
2022-04-18 17:23:19 +0700

    deloy

--- android/app/src/main/kotlin/com/example/may_tinh_bo_tui/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/may_tinh_bo_tui/MainActivity.kt
@@ -1,6 +1,20 @@
 package com.example.may_tinh_bo_tui
 
 import io.flutter.embedding.android.FlutterActivity
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+import com.microsoft.appcenter.distribute.Distribute;
+import android.os.Bundle;
 
 class MainActivity: FlutterActivity() {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    
+    AppCenter.start(getApplication(), "OUR SECRET KEY", Distribute.class);
+    
+    AppCenter.setEnabled(true);
+    
+    super.onCreate(savedInstanceState); 
 }
