9bb4bcf256db755cb88dd014dfa935613ae51467
25042001iueH <hieutdc@antoree.com>
2022-04-18 16:47:09 +0700

    deloy-1

--- android/app/src/main/kotlin/com/example/may_tinh_bo_tui/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/may_tinh_bo_tui/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.may_tinh_bo_tui
 
 import io.flutter.embedding.android.FlutterActivity
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 class MainActivity: FlutterActivity() {
 }
